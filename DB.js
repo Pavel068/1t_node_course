f73b5df11387e70c297766fc44c393e8179c10b7
Pavel068 <trololo011@gmail.com>
2023-02-06 01:06:08 +0300

    New branch

--- DB.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('app', 'root', 'yahz', {
-    host: 'localhost',
-    dialect: 'mysql'
-});
-
-module.exports = sequelize;
