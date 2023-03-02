6db7c186c068e3badb24422917d6d37506ad6961
Pavel068 <trololo011@gmail.com>
2023-03-02 19:52:08 +0300

    Socket Chat

--- server/app.js
+++ server/app.js
@@ -1,12 +1,19 @@
+require('dotenv').config({});
+
 const express = require('express');
 const { createServer } = require('http');
 const { Server } = require('socket.io');
+const { init } = require('./models/init');
+const ChatService = require('./services/ChatService');
+
+const chatService = new ChatService();
 
 const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
     cors: {
-        origin: "http://localhost:8080"
+        origin: ['http://localhost:8080'],
+        credentials: true
     }
 });
 
@@ -17,24 +24,47 @@ app.get('/', async (req, res) => {
 
 app.listen(3000, async () => {
     console.log('Server started');
+    await init();
 });
 
 // Запуск сокет-сервера
-io.use((socket, next) => {
-    const token = socket.handshake.auth.token;
-    if (token === 'secret') {
-        next();
-    } else {
-        const err = new Error("not authorized");
-        err.data = { content: "Please retry later" };
-        next(err);
-    }
-});
+io.on('connection', async (socket) => {
+    // Получить и отправить историю
+    socket.emit('history', {});
 
-io.on('connection', (socket) => {
-    socket.on('message', (data) => {
-        socket.emit('message', data);
-    })
+    // Комнаты
+    socket.on('create_room', async data => {
+        // Создать комнату в БД
+        try {
+            const room = await chatService(data);
+
+            // Отправить список комнат
+            socket.emit('rooms_list_changed', room);
+        } catch (e) {
+            console.error(e);
+        }
+    });
+    socket.on('join_room', data => {
+        // Получить историю комнаты
+    });
+    socket.on('leave-room', data => {
+        socket.leave('room-' + data.room_id);
+    });
+
+    // Сообщения
+    socket.on('message', data => {
+        if (data.room_id) {
+            io.to('room-' + data.room_id).emit('message', {
+                name: data.name,
+                message: data.message
+            });
+        } else {
+            socket.broadcast.emit('message', {
+                name: data.name,
+                message: data.message
+            });
+        }
+    });
 });
 
 httpServer.listen(3001);
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,47 +1,120 @@
 <template>
   <div class="home">
-    <input type="number" v-model="room_id" />
-    <input type="text" v-model="message">
-    <button class="btn" @click="send">Отправить</button>
-    <div class="chat"></div>
+    <div class="wrapper">
+      <div class="chat">
+        <p>Чат</p>
+        <div class="chat-history">
+          <div class="chat-history-item" v-for="message in messages">
+            <p><b>{{ message.name }}</b></p>
+            <p>{{ message.message }}</p>
+          </div>
+        </div>
+        <p>Сообщение</p>
+        <div style="text-align: left">
+          <input type="text" placeholder="Ваше имя" style="margin-left: 5px;" v-model="author">
+        </div>
+        <div>
+          <input type="text" style="width: 250px;" v-model="message">
+          <button @click="sendMessage">Отправить</button>
+        </div>
+      </div>
+      <div class="rooms">
+        <p>Комнаты</p>
+        <div class="rooms-list">
+            <div class="rooms_list_item" v-for="room in rooms">
+                <p @click="joinRoom(room.id)">{{ room.name }}</p>
+            </div>
+        </div>
+        <p>Создать комнату</p>
+        <div>
+          <input type="text" style="width: 250px;" v-model="creating_room" />
+          <button @click="createRoom">Создать</button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
+<style scoped>
+.wrapper {
+  width: 800px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  border: 1px solid gray;
+}
+
+.chat-history,
+.rooms-list {
+  width: 350px;
+  height: 300px;
+  background-color: #f5f5f5;
+  overflow-y: scroll;
+}
+
+.chat-history-item {
+  padding: 10px;
+  border: 1px solid #bebebe;
+  text-align: left;
+}
+</style>
+
 <script>
 const { io } = require("socket.io-client");
-const socket = io("http://localhost:3001", {
-  auth: {
-    token: 'secret'
-  }
-});
-
-socket.on('message', (data) => {
-  console.log(data);
-})
-socket.on('connect_error', (data) => {
-  console.error(data);
-})
-
-import HelloWorld from '@/components/HelloWorld.vue'
+const socket = io("http://localhost:3001");
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  },
   data() {
     return {
+      messages: [],
+      author: null,
       message: null,
-      room_id: 1
+      rooms: [],
+      room_id: null,
+      creating_room: null
     }
   },
   methods: {
-    send() {
-      socket.emit('message', {
+    createRoom() {
+      socket.emit('create_room', {
+        name: this.creating_room
+      });
+      this.creating_room = null;
+    },
+    joinRoom(room_id) {
+      if (this.room_id) {
+        socket.emit('leave_room', {room_id: this.room_id});
+      }
+      
+    },
+    sendMessage() {
+      const message = {
+        name: this.author,
         message: this.message,
         room_id: this.room_id
-      });
+      }
+
+      socket.emit('message', message);
+
+      this.messages.push(message);
+
+      this.message = null;
     }
+  },
+  mounted() {
+    socket.on('connect_error', (data) => {
+      console.error(data);
+    })
+
+    socket.on('history', data => {});
+    socket.on('rooms_list_changed', data => {
+      console.log(data);
+    });
+
+    socket.on('message', data => {
+      this.messages.push(data);
+    });
   }
 }
 </script>
