6f5cf936e34c58965490f7e8076fd5a38968713b
Pavel068 <trololo011@gmail.com>
2023-03-24 10:15:09 +0300

    Install GraphQL Apollo

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { CategoryModule } from './category/category.module';
+
+@Module({
+  controllers: [AppController],
+  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: true,
+      autoSchemaFile: true
+    }),
+    CategoryModule,
+  ]
+})
+export class AppModule {}
